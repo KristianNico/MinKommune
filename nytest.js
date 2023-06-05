00ba9a5062818a2b447cf33238f9d680bd97419f
KristianNico <82471901+KristianNico@users.noreply.github.com>
2023-06-05 11:43:11 +0200

    Update nytest.js

--- nytest.js
+++ nytest.js
@@ -1,6 +1,6 @@
 function hilsen() {
   var navn = "Kristian";
   var alder = 30;
-  var besked = "Hej, " + navn + "! Du er " + alder + " år gammel.";
+  var besked = "Davs, " + navn + "! Du er " + alder + " år gammel.";
   alert(besked);
 }
