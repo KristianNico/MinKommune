e96dec567f04c01ba7b0ca43d6c42be195284fcc
KristianNico <82471901+KristianNico@users.noreply.github.com>
2023-06-05 11:58:30 +0200

    Add files via upload

---
+++ VITester.js
@@ -0,0 +1,7 @@
+// @ts-mime-type text/javascript
+function hilsen() {
+  var navn = "Kristian";
+  var alder = 30;
+  var besked = "Davs, " + navn + "! Du er " + alder + " år gammel.";
+  alert(besked);
+}
