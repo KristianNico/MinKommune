c20c8a78e045d0f5eead5206642a34e9f7709f7c
KristianNico <82471901+KristianNico@users.noreply.github.com>
2023-06-05 20:46:03 +0200

    Delete nytest.js

--- nytest.js
+++
@@ -1,7 +0,0 @@
-// @ts-mime-type text/javascript
-function hilsen() {
-  var navn = "Kristian";
-  var alder = 30;
-  var besked = "Davs, " + navn + "! Du er " + alder + " år gammel.";
-  alert(besked);
-}
