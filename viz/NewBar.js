b3eca87f251a76eb4d7ca2b62f768c05d700836f
Kristian Nicolaisen <kristian.nico@gmail.com>
2023-06-10 10:07:45 +0200

    fix

--- viz/NewBar.js
+++ viz/NewBar.js
@@ -2,6 +2,9 @@ function generateGraph1() {
   // URL til JSON-data
 const url = 'https://KristianNico.github.io/MinKommune/data/KVPERS21.JSON';
 
+// Antal værdier, der skal vises
+const numValuesToShow = 10;
+
 // Opret SVG-elementet
 const svg = d3.select("#Graph1");
 const svgContainer = d3.select('#container');
@@ -13,19 +16,27 @@ d3.json(url).then(function(data) {
         d.INDHOLD = +d.INDHOLD;
     });
 
+    // Sorter data efter værdi (laveste til højeste)
+    data.sort(function(a, b) {
+      return a.INDHOLD - b.INDHOLD;
+  });
+
+  // Behold kun de 10 laveste værdier
+  const filteredData = data.slice(0, numValuesToShow);
+
     // Skab skalaer til x- og y-aksen
     const xScale = d3.scaleBand()
-        .domain(data.map(function(d) { return d.KANDIDAT; }))
+        .domain(filteredData.map(function(d) { return d.KANDIDAT; }))
         .range([0, 400])
         .padding(0.1);
 
     const yScale = d3.scaleLinear()
-        .domain([0, d3.max(data, function(d) { return d.INDHOLD; })])
+        .domain([0, d3.max(filteredData, function(d) { return d.INDHOLD; })])
         .range([300, 0]);
 
     // Opret bjælkerne i barchart
     svg.selectAll("rect")
-        .data(data)
+        .data(filteredData)
         .enter().append("rect")
         .attr("class", "bar")
         .attr("x", function(d) { return xScale(d.KANDIDAT); })
